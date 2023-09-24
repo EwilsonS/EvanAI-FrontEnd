4864f79f6f6bdd02509152bfb1355e8b16ceb4ce
Evan Wilson <ewilsons@aol.com>
2023-09-24 18:02:19 -0400

    chat initial laod bug

--- client/src/components/Chat.svelte
+++ client/src/components/Chat.svelte
@@ -1,11 +1,12 @@
 <script>
-  import { onMount } from "svelte";
+  import { onMount, beforeUpdate, afterUpdate } from "svelte";
   import { loadConversationHistory } from "../chat_script2";
 
   export let agent = ''
 
   // Load the conversation history when the page loads
   onMount(() => loadConversationHistory());
+
   const queryParams = new URLSearchParams(location.search);
   queryParams.set('agent', agent);
   history.replaceState({}, '', `${location.pathname}?${queryParams}`);
