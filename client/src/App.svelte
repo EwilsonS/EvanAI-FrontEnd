e69e914758e1ffbeebe7d2d37c302a5cc90bcdbc
Evan Wilson <ewilsons@aol.com>
2023-09-24 19:57:53 -0400

    update meta for blank screen

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -8,7 +8,7 @@
 
 <svelte:head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>EvanAI</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" />
