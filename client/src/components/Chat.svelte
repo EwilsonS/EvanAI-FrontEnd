f2daf4cbf91311260142bb03320cb0549dd52fe9
Evan Wilson <ewilsons@aol.com>
2023-09-24 20:43:33 -0400

    blank screen bug fix

--- client/src/components/Chat.svelte
+++ client/src/components/Chat.svelte
@@ -15,35 +15,3 @@
 <div id="chat-box">
   <div id="chat-container2" />
 </div>
- <style>
-    #chat-box {
-    height: 90vh;
-    justify-content: center;
-    flex: 1;
-    margin: 0 auto;
-    width: 100vw; /* Set width to 100vh to make it the same as the height */
-    box-sizing: border-box;
-    background-color: #343541;
-    position: relative;
-    min-height: 81vh;
-    align-items: unset;
-    border-bottom: #2a2b32;
-    border-top: #2a2b32;
-  }
-
-  #chat-container2 {
-    height: 90vh;
-    justify-content: center;
-    flex: 1;
-    margin: 0 auto;
-    width: 100vw; /* Set width to 100vh to make it the same as the height */
-    box-sizing: border-box;
-    background-color: #004549;
-    position: relative;
-    min-height: 81vh;
-    align-items: unset;
-    border-bottom: #2a2b32;
-    border-top: #2a2b32;
-    overflow-y: auto;
-  }
- </style>
